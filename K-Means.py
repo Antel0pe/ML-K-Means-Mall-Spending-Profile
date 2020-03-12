1c21380cab8b4f3d7f859be4483e123d080f639f
AnishPhadnis <38162659+AnishPhadnis@users.noreply.github.com>
2020-03-11 22:27:31 -0400

    Updated kmeans.py

--- K-Means.py
+++ K-Means.py
@@ -10,17 +10,17 @@ dataset = pd.read_csv('mall.csv')
 X = dataset.iloc[:, [3, 4]].values
 # y = dataset.iloc[:, 3].values
 
-"""# Splitting the dataset into the Training set and Test set
+# Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 # Feature Scaling
-"""from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)"""
+y_train = sc_y.fit_transform(y_train)
 
 # Using the elbow method to find the optimal number of clusters
 from sklearn.clusters import KMeans
